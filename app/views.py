61a7311ce460c8e78e53e8194908c11e49e5b33c
Daniel Mendes <senseidanielmendes@gmail.com>
2023-06-05 23:53:01 -0300

    add new stuff

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {'title':'Daniel Mendes',
-               'copyrigth': 'Copyrigth Daniel Mendes'}
+               'copyrigth': 'Copyrigth Daniel Mendes',
+               'curriculo':'O que eu sei:'}
     return render(request, 'index.html', context)
 # Create your views here.
